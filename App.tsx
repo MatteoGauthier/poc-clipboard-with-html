6b056339cfc2f163fc130cd059052e16f191786e
MatteoGauthier <matteo.gauthier@gmail.com>
2022-11-28 11:46:54 +0100

    Add new alternative size for the quick link preview

--- App.tsx
+++ App.tsx
@@ -85,7 +85,7 @@ export default function App() {
                     fontFamily:
                       "'Poppins', 'system-ui', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'Roboto', 'sans-serif'",
                     margin: "0",
-                    
+
                     lineHeight: "100%",
                     display: "block",
                     width: 240,
@@ -103,6 +103,65 @@ export default function App() {
               <td align="right">
                 <img
                   style={{ display: "block" }}
+                  src="https://user-images.githubusercontent.com/32040951/204016684-6d7edd07-c95e-4921-953d-3c3448044501.svg"
+                  alt="Blink logo"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </a>
+      <hr />
+      <a
+        autoCorrect="off"
+        autoCapitalize="off"
+        spellCheck="false"
+        title={`Télécharger le fichier "${fileName}"`}
+        style={{
+          textDecoration: "none",
+          textDecorationColor: "#3389FF",
+          padding: 8,
+          display: "inline-block",
+          border: "1.5px solid #f3f3f3",
+          borderRadius: 12,
+          width: 240,
+        }}
+        href="https://inablink.io/RZD6qwZ9W8HrOvr6gODq#BdqmUpeRRGSxneIq62Mu3w"
+      >
+        <img
+          width="240"
+          style={{ borderRadius: 8 }}
+          src="https://user-images.githubusercontent.com/32040951/203973885-071abf8c-b09a-46eb-b57b-3fdb614b984e.jpg"
+          alt="Your document preview button"
+        />
+        <table style={{ borderSpacing: "0", marginTop: 2 }} width="100%">
+          <tbody>
+            <tr>
+              <td>
+                <p
+                  style={{
+                    fontFamily:
+                      "'Poppins', 'system-ui', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'Roboto', 'sans-serif'",
+                    margin: "0",
+
+                    lineHeight: "100%",
+                    display: "block",
+                    width: 160,
+                    maxWidth: 160,
+                    color: "#3389FF",
+                    textDecoration: "none",
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    fontSize: ".9em",
+                  }}
+                >
+                  {fileName}
+                </p>
+              </td>
+              <td align="right">
+                <img
+                  style={{ display: "block", height: ".9em" }}
                   src="https://user-images.githubusercontent.com/32040951/204016684-6d7edd07-c95e-4921-953d-3c3448044501.svg"
                   alt="Blink logo"
                 />
